9a740872185e80be355360d3ea1391cbdc4a3b82
shimaokasonse <simaokasonse@yahoo.co.jp>
2017-01-18 12:24:35 +0900

    train

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
 import argparse
 from util import get_batch, decode
 from model import Model
@@ -8,31 +9,31 @@ from model import Model
 dataset = joblib.load("data.pkl")
 dicts   = joblib.load("dicts.pkl")
 W, E, P = dataset["W"], dataset["E"], dataset["P"]
-W_train, E_train, P_train = W[:9000000], E[:9000000], P[:9000000]
-W_valid, E_valid, P_valid = W[9000000:], E[9000000:], P[9000000:]
-model = Model(input_length=60,output_length=8,w_dim=100,e_dim=80,p_dim=20,enc_dim=100,dec_dim=100,w_size=200000,e_size=752,p_size=62)
-#model.load("model.ckpt")
+ts = 7000000
+print W.shape
+W_train, E_train, P_train = W[:ts], E[:ts], P[:ts]
+W_valid, E_valid, P_valid = W[ts:], E[ts:], P[ts:]
+model = Model(input_length=60,output_length=8,w_dim=300,e_dim=200,p_dim=100,enc_dim=300,dec_dim=300,w_size=200000,e_size=750,p_size=62)
+model.load("model.ckpt")
 
 loss_train = 0.
-for i in range(100000):
+for i in range(10000000):
     W_b, E_b, P_b = get_batch(W_train,E_train,P_train,1000)
     loss_train += model.train(W_b,E_b,P_b)
-    
-    if i % 500 == 0:
-        loss_valid = 0.
-        for j in range(10):
-            W_b, E_b, P_b = get_batch(W_valid,E_valid,P_valid,1000)
-            loss_valid += model.error(W_b,E_b,P_b)
-            print "epoch:" , str(i/500)
-            print "train:",loss_train/500.
-            print "valid:",loss_valid/10.
-            loss_train = 0.
-            print "--- true ----"
-            W_b, E_b, P_b = get_batch(W_valid,E_valid,P_valid,1)
-            E_sample, P_sample = model.sample(W_b)
-            decode(W_b,E_b,P_b,dicts["id2word"],dicts["id2emoji"])
-            print "--- sample ---"
-            decode(W_b,E_sample,P_sample,dicts["id2word"],dicts["id2emoji"])
-            print "---"*20
-            model.save("model.ckpt")
+    if i % 100 == 0:
+        W_b, E_b, P_b = get_batch(W_valid,E_valid,P_valid,1000)
+        #W_b, E_b, P_b = get_batch(W_train,E_train,P_train,1000)
+        loss_valid = model.error(W_b,E_b,P_b)
+        print "epoch:" , str(i/100)
+        print "train:",loss_train/100.
+        print "valid:",loss_valid
+        loss_train = 0.
+        print "--- true ----"
+        W_b, E_b, P_b = get_batch(W_train,E_train,P_train,1)
+        E_sample, P_sample = model.sample(W_b)
+        decode(W_b,E_b,P_b,dicts["id2word"],dicts["id2emoji"])
+        print "--- sample ---"
+        decode(W_b,E_sample,P_sample,dicts["id2word"],dicts["id2emoji"])
+        print "---"*20
+        model.save("model.ckpt")
                                                                                                                                                                                                                                                                                     
